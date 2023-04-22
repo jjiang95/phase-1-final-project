a029932b7fa46ededcf811ec371575b56d08b1d8
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-04-22 19:20:07 -0400

    implemented search functionality

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     // https://www.thecocktaildb.com/api/json/v1/1/search.php?s=
     let form = document.querySelector("form");
-    form.addEventListener("submit", () => {
-        
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let searchText = e.target.children[0].value;
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`)
+        .then(resp => resp.json())
+        .then(json => {
+            json.drinks.forEach(drink => console.log(drink))
+        })
     })
 })
