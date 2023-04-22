7b86e04732382d2a29af602b8ad1ae6e190c6cc1
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-04-22 19:00:00 -0400

    HTML complete, JS started

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // https://www.thecocktaildb.com/api/json/v1/1/search.php?s=
+    let form = document.querySelector("form");
+    form.addEventListener("submit", () => {
+        
+    })
+})
