1eaa37b74c2bb93102d6665643651fcab17320d2
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-04-25 17:37:29 -0400

    added flip card mechanic

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {
     let form = document.querySelector("form");
+    //When form is submitted, a fetch request is made to the API 
+    //to retrieve elements from the database depending on search-text input
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         let searchText = e.target.children[0].value;
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`)
         .then(resp => resp.json())
         .then(json => {
-            json.drinks.forEach(drink => createCard(drink))
+            json.drinks.forEach(drink => createCard(drink)) //Create a formatted card for each result fetched
         })
         // form.reset();
     })
@@ -21,8 +21,19 @@ function createCard(drink) {
     name.textContent = drink.strDrink;
     let instructions = document.createElement("p");
     instructions.textContent = drink.strInstructions;
+    //Prepend rather than append, so that newer results always show first
     document.querySelector("#gallery").prepend(card)
-    card.appendChild(name)
-    card.appendChild(instructions);
-
+    //Add content for front and back of card
+    let cardFront = document.createElement("div");
+    cardFront.classList.add("front");
+    cardFront.appendChild(name);
+    let cardBack = document.createElement("div")
+    cardBack.classList.add("back");
+    cardBack.appendChild(instructions)
+    card.appendChild(cardFront)
+    card.appendChild(cardBack);
+    //Add event listener to flip card when clicked
+    card.addEventListener("click", () => {
+        card.classList.toggle("flipCard");
+    })
 }
