07c09c8e479647a203c85e75e545d21dca2a3a19
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-04-28 12:38:55 -0400

    removes all uses of innerHTML and replaces with manual JS methods to create, remove, and populate HTML elements

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form1 = document.querySelector("#by-name");
-    //When form1 is submitted, a fetch request is made to the API 
-    //to retrieve elements from the database depending on search-text input
     form1.addEventListener("submit", (e) => {
         e.preventDefault();
         let searchText = e.target.children[0].value;
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`)
         .then(resp => resp.json())
         .then(json => {
-            json.drinks.forEach(drink => createCard(drink)) //Create a formatted card for each result fetched
+            json.drinks.forEach(drink => createCard(drink));
         })
         form1.reset();
     })
 
     const form2 = document.querySelector("#by-ingredient");
-    //Same mechanic as form1, but using a different search feature of the API
     form2.addEventListener("submit", (e) => {
         e.preventDefault();
         let searchText = e.target.children[0].value;
@@ -22,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(resp => resp.json())
         //The 'lookup by ingredient' feature in the API only returns a name, a picture, and an ID
         //and doesn't return ingredients or instructions
-        //So another fetch request is chained on to look up the cocktail by that ID via the 'lookup by ID' feature
+        //So another fetch request is chained on to look up the cocktail by its ID via the 'lookup by ID' feature
         //to retrieve the data needed
         .then(json => {
             json.drinks.forEach(drink => {
@@ -37,7 +34,10 @@ document.addEventListener("DOMContentLoaded", () => {
     //Clear the DOM of all search results
     const button1 = document.querySelector("#clear-gallery");
     button1.addEventListener("click", () => {
-        document.querySelector("#gallery").innerHTML = ``;
+        const gallery = document.querySelector("#gallery");
+        while (gallery.firstChild) {
+            gallery.removeChild(gallery.lastChild);
+        } 
     }) 
 
     //Generate a random cocktail from the API
@@ -52,36 +52,28 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function createCard(drink) {
-    const card = document.createElement("div")
-    card.classList.add("card");
+    const card = document.createElement("div")    
+    const cardFront = document.createElement("div");    
+    const cardBack = document.createElement("div")
+    
+    card.classList.add("card");    
     card.style.backgroundImage = `url(${drink.strDrinkThumb})`
-    card.innerHTML = `
-        <div class="front">
-            <h1>${drink.strDrink}</h1>
-        </div>
-        <div class="back">
-            <p>${extractIngredientsIntoString(drink)}</p>
-            <br/>
-            <p>${drink.strInstructions}</p>
-        </div>
-    `
-    // let name = document.createElement("h1");
-    // name.textContent = drink.strDrink;
-    // let instructions = document.createElement("p");
-    // instructions.textContent = drink.strInstructions;
+    cardFront.classList.add("front");    
+    cardBack.classList.add("back");
     
-    //Prepend rather than append, so that newer results always show first
+    const name = document.createElement("h1");
+    name.textContent = drink.strDrink;
+    const instructions = document.createElement("p");
+    instructions.textContent = drink.strInstructions;
+    const measurements = document.createElement("p");
+    measurements.textContent = extractIngredientsIntoString(drink);
+
+    card.append(cardFront, cardBack);
+    cardFront.appendChild(name)
+    cardBack.appendChild(measurements);
+    cardBack.appendChild(instructions);
     document.querySelector("#gallery").prepend(card)
-    //Add content for front and back of card
-    // let cardFront = document.createElement("div");
-    // cardFront.classList.add("front");
-    // cardFront.appendChild(name);
-    // let cardBack = document.createElement("div")
-    // cardBack.classList.add("back");
-    // cardBack.appendChild(instructions)
-    // card.append(cardFront, cardBack);
     
-    //Add event listener to flip card when clicked
     card.addEventListener("click", () => {
         card.classList.toggle("flipCard");
     })
@@ -98,13 +90,15 @@ function extractIngredientsIntoString(drink) {
     for (let i=0; i < ingredients.length;i++) {
         finalObject[`${ingredients[i]}`] = `${measurements[i]}`;
     }
+
     for (const key in finalObject) {
         //If there is no measurement available for an ingredient, the key's value is emptied so that it doesn't display
         if (finalObject[key] === "undefined") {
             finalObject[key] = "";
         }
-        ingredientsString += `${finalObject[key]} ${key}, `
-    };
+        ingredientsString += `${finalObject[key]} ${key}, `;
+    }
+
     //Remove comma and space at the end of the ingredients string
     return ingredientsString.substring(0, ingredientsString.length - 2)
 
